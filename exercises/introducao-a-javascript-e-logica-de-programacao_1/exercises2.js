182498b1da9c7927f41e519afe29e1baad499a9d
Wallacy Francis <wallacy_francis@hotmail.com>
2021-05-20 16:35:01 -0300

    add exercicios

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises2.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Protifólio', 'Links'];
+let menuServices = menu[1];
+
+/* console.log(menuServices); */
+
+let indefOfPortifolio = menu.indexOf('Protifólio');
+/* console.log(indefOfPortifolio); */
+
+menu.push('Contato');
+console.log(menu);
+
+
