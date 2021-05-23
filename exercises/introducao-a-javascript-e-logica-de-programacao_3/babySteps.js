f21b3e594486f425847035f06f2eae8adca6579d
Wallacy Francis <wallacy_francis@hotmail.com>
2021-05-23 14:41:27 -0300

    Realizado exercico 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/babySteps.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/babySteps.js
@@ -1,4 +1,4 @@
-let fruits = [3, 4, 10, 1, 12];
+/* let fruits = [3, 4, 10, 1, 12];
 let sum = 0;
 
 for (let i = 0 ; i > fruits.length ; i++) {
@@ -9,5 +9,13 @@ for (let i = 0 ; i > fruits.length ; i++) {
 if (sum > 15) {
   console.log(sum)
 } else {
-  console.log('valor menor que 16');
-}
+  console.log('valor menor que 16'); */
+
+  let n = 9;
+  let result = 100;
+
+  for (let i = 0; i <= n ; i++) {
+    result -= i;
+  }
+
+  console.log(result);
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercises-course-4-3.js
@@ -0,0 +1,46 @@
+/* 1 - Programa que imprime numero de asteriscos */
+
+/* let n = 5;
+let s = '';
+let c = '*';
+
+for (let i = 0 ; i < n ; i++) {
+  s = s + c;
+}
+
+for (let i = 0 ; i < n ; i++){
+  console.log(s)
+} */
+
+/* 2 - Piramede de asteriscos */
+/* let n = 5;
+let c = '*';
+let s = '';
+
+for (let i = 0 ; i < n ; i++) {
+  s = s + c;
+  console.log(s);
+} */
+
+/* 3 - Piramede de asteriscos inversa */
+/* let n = 10;
+let s = '';
+let c = '*';
+
+for (let i = 0 ; i < n ; i++) {
+  s += ' ';
+}
+
+for (let i = 0 ; i < n ; i++) {
+  s += c;
+  console.log(s);
+} */
+
+/*  */
+
+
+
+
+
+
+
